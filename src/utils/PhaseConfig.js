25774ecf6fd0ca1fae272ea325a4ff923ab90d3f
GrokMultiverse <skyrider.protonmail@gmail.com>
2025-12-27 04:20:11 +0600

    PhaseConfig.js

--- src/utils/PhaseConfig.js
+++ src/utils/PhaseConfig.js
@@ -1,13 +1,14 @@
 export const PhaseConfig = {
   isPointsFarmingLive: true,
-  isTestnetNftMintComing: true, // "Coming Soon"
-  isMainnetNftLocked: true,     // "Locked"
-  isAirdropLocked: true,        // "Locked"
+  isTestnetNftMintComing: true,
+  isMainnetNftLocked: true,
+  isAirdropLocked: true,
+  adminWallet: "8LMWS1r1hyqiRv94TFKA1pVUGmxGQQJCQHohZEmiUXN8" 
 };
 
 export const SOCIALS = {
   x: "https://x.com/GrokMultiverse",
   devX: "https://x.com/0xlmamun",
   tg: "https://t.me/grokmultiverse",
-  faucet: "https://faucet.quicknode.com/solana/devnet" // or official solana faucet
+  faucet: "https://faucet.quicknode.com/solana/devnet"
 };
