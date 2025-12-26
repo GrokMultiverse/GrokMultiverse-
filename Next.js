8300e9ff8f24556110c39e55524fda36dc81313c
GrokMultiverse <skyrider.protonmail@gmail.com>
2025-12-27 02:52:59 +0600

    Next.js

---
+++ Next.js
@@ -0,0 +1,6 @@
+npx create-next-app@latest grokmultiverse
+# (Select: Yes to Tailwind, Yes to App Router, No to TypeScript for simplicity)
+
+cd grokmultiverse
+
+npm install @solana/web3.js @project-serum/anchor @solana/wallet-adapter-react @solana/wallet-adapter-react-ui @solana/wallet-adapter-wallets @solana/wallet-adapter-base framer-motion lucide-react
