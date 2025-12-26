ada6223b1ab28081e034265d1572c7a04ae2565c
GrokMultiverse <skyrider.protonmail@gmail.com>
2025-12-27 03:48:05 +0600

    layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'GrokMultiverse',
+  description: 'Solana NFT Project',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
