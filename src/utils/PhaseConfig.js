92684982947184204074f49c55b8c7d76b53841c
GrokMultiverse <skyrider.protonmail@gmail.com>
2025-12-27 02:40:45 +0600

    PhaseConfig.js

---
+++ src/utils/PhaseConfig.js
@@ -0,0 +1,13 @@
+export const PhaseConfig = {
+  isPointsFarmingLive: true,
+  isTestnetNftMintComing: true, // "Coming Soon"
+  isMainnetNftLocked: true,     // "Locked"
+  isAirdropLocked: true,        // "Locked"
+};
+
+export const SOCIALS = {
+  x: "https://x.com/GrokMultiverse",
+  devX: "https://x.com/0xlmamun",
+  tg: "https://t.me/grokmultiverse",
+  faucet: "https://faucet.quicknode.com/solana/devnet" // or official solana faucet
+};
